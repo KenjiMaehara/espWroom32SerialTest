029dc597ea0df1acc405350a7f9ba88054e4ef25
KenjiMaehara <kmaehara@mel.co.jp>
2020-02-16 18:40:24 +0900

    UART1のピンアサインの変更に成功。

--- test20200215/test20200215.ino
+++ test20200215/test20200215.ino
@@ -1,13 +1,13 @@
-String sendstr = "arduino001";
+String sendstr = "arduino000";
+String sendstr01 = "arduino001";
 String sendstr02 = "arduino002";
-
 
 void setup() {
   // initialize both serial ports:
 
   #if 1
   Serial.begin(9600);
-  //Serial1.begin(9600);
+  Serial1.begin(9600,SERIAL_8N1,4,0);
   Serial2.begin(9600);
   #endif
 
@@ -21,6 +21,9 @@ void loop() {
        Serial.write(sendstr.charAt(i)); 
     }
 
+    for (int i = 0; i < sendstr01.length(); i++){
+       Serial1.write(sendstr01.charAt(i)); 
+    }
 
     for (int i = 0; i < sendstr02.length(); i++){
        Serial2.write(sendstr02.charAt(i)); 
